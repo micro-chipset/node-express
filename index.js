4635af9cd05fd2c8ca666c36253d3f199f02c170
micro-chipset <tyrnov.ivan@gmail.com>
2020-04-05 08:16:42 +0300

    firts commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
