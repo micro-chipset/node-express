e25d0cc97b98e2911b6f5109c8f66e87b97bf350
micro-chipset <tyrnov.ivan@gmail.com>
2020-04-13 19:05:57 +0300

    add js script format price

--- routes/courses.js
+++ routes/courses.js
@@ -3,9 +3,7 @@ const Course = require('../models/course')
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const courses = await Course.getAll()
-    console.log(courses);
-    
+    const courses = await Course.getAll()    
 
     res.render('courses', {
         title: 'Курсы',
