d5fdc06dfd3c4ce00351324190c8457a0eb286f1
micro-chipset <tyrnov.ivan@gmail.com>
2020-04-17 09:00:54 +0300

    add mongoose- add connect mongoo db- add Scheme course- edit router course

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
@@ -27,6 +28,17 @@ app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+async function start() {
+    try {
+        const url = `mongodb+srv://chipset:kasmxAHavXf1RHAU@cluster0-amzkb.mongodb.net/shop`
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
